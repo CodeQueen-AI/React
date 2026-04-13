4455088fb3c4a2c69fad44f0534363725fede56e
CodeQueen-AI <codeq209@gmail.com>
2026-04-13 18:28:09 +0500

    update

---
+++ app/src/SWR/components/Users.jsx
@@ -0,0 +1,24 @@
+import useSWR from "swr";
+import { fetchUsers } from "../api/usersApi";
+
+const fetcher = () => fetchUsers();
+
+function Users() {
+  const { data, error, isLoading } = useSWR("users", fetcher);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading data</p>;
+
+  return (
+    <div>
+      <h2>Users List</h2>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
---
+++ app/src/TanStack Query/components/Users.jsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchUsers } from "../api/usersApi";
+
+function Users() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading data</p>;
+
+  return (
+    <div>
+      <h2>Users List</h2>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
