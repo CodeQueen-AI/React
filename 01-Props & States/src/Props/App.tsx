8f9d9ae52100b6ba650517d4f73267216814e943
CodeQueen-AI <codeq209@gmail.com>
2026-04-14 20:49:15 +0500

    update

--- 01-Props & States/src/Props/App.tsx
+++ 01-Props & States/src/Props/App.tsx
@@ -1,4 +1,4 @@
-import User from "./components/User"
+import User from "./User"
 
 function App() {
   return (
--- 03-Conditional Rendering/src/App.tsx
+++ 03-Conditional Rendering/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1>Conditional Rendering</h1>
 
-      {isLoggedIn ? <h2>Welcome 👋</h2> : <h2>Please Login</h2>}
+      {isLoggedIn ? <h2>Welcome! </h2> : <h2>Please Login</h2>}
     </div>
   );
 }
